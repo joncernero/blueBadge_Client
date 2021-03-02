8680933497cc5b562065ca0ac5c0eb85342036ca
jesnadolski <nadolski.jessica@gmail.com>
2021-03-02 18:03:12 -0500

    adding Height search and PlantIndex routing

--- src/components/PlantIndex.js
+++ src/components/PlantIndex.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import SearchPlants from "./SearchPlants";
 import UserSearchPlants from "./UserSearchPlants";
 import FlowerSearch from "./FlowerSearch";
+import SearchHeight from "./SearchHeight";
 import {BrowserRouter, Route, Link, Switch} from 'react-router-dom';
 
 const PlantIndex = (props) => {
@@ -14,6 +15,7 @@ const PlantIndex = (props) => {
         <li><Link to="/UserSearchPlants">Search by Plant Name</Link></li>
         <li><Link to="/FlowerSearch">Search by Flower Color</Link></li>
         <li><Link to="/SearchPlants">View Plants Alphabetically</Link></li>
+        <li><Link to="/SearchHeight">Search By Height</Link></li>
     </ul>
 </div>
 <div>
@@ -22,6 +24,7 @@ const PlantIndex = (props) => {
 <Route exact path="/UserSearchPlants"><UserSearchPlants token={props.token}/></Route>
 <Route exact path="/FlowerSearch"><FlowerSearch token={props.token}/></Route>
 <Route exact path="/SearchPlants"><SearchPlants token={props.token}/></Route>
+<Route exact path="/SearchHeight"><SearchHeight token={props.token}/></Route>
 </Switch>
 
 
