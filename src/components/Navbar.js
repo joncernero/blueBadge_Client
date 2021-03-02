51c444085c93ada8a0d8b08f07d7ebd93a2b941e
Jonathan Cernero <cernero.jon@gmail.com>
2021-03-01 20:36:19 -0500

    updating navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,19 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Button, NavHeader } from '../components/styled'
 
 const Toggler = props => {
   const [isOpen, setIsOpen] = useState(false)
-  const [user, setUser] = useState('')
   console.log(props.token)
   const toggle = () => {
     let newIsOpen = !isOpen
     setIsOpen(newIsOpen)
   }
 
-  function getUser() {
-    const user = JSON.parse(localStorage.getItem('user'))
-  }
-
-  useEffect(() => {
-    getUser()
-  }, [])
-
   return (
     <div>
       <NavHeader>
         <div href='/'>iPlants.com</div>
-        <h1>Welcome, {user.firstName}</h1>
         {/* <NavbarToggler onClick={toggle} className='mr-2' /> */}
         {/* <Collapse isOpen={!collapsed} navbar> */}
         <ul>
