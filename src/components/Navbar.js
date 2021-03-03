e882ef3146aabe0ef04e88fca991a4d748346844
Bonita Scott <bonitalscott@yahoo.com>
2021-03-02 21:36:24 -0500

    Stash temp code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,40 @@
-import React, {useState} from "react";
-import {BrowserRouter, Route, Link, Switch} from 'react-router-dom';
-import SearchPlants from "./SearchPlants";
-import UserSearchPlants from "./UserSearchPlants";
-import FlowerSearch from "./FlowerSearch";
-import PlantIndex from "./PlantIndex";
-import Home from "./Home";
-import {Button, NavbarToggler, Collapse} from 'reactstrap';
-
-
-
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+import SearchPlants from './SearchPlants';
+import UserSearchPlants from './UserSearchPlants';
+import FlowerSearch from './FlowerSearch';
+import PlantIndex from './PlantIndex';
+import Home from './Home';
+import { Button, NavbarToggler, Collapse } from 'reactstrap';
 
 const Navbar = (props) => {
-    const [isOpen, setIsOpen] = useState(false)
-    const toggle = () => {
-        let newIsOpen = !isOpen 
-        setIsOpen(newIsOpen)
-    }
-    return ( 
-        <div>
-
-        <div>
-            <ul>
-                <li><Link to="/Dashboard">My Garden</Link></li>
-                <li><Link to="/PlantIndex">Explore Plants</Link></li>
-                {/* <li><Link to="/SearchPlants">View All Plants</Link></li>
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => {
+    let newIsOpen = !isOpen;
+    setIsOpen(newIsOpen);
+  };
+  return (
+    <div>
+      <div>
+        <ul>
+          <li>
+            <Link to='/Dashboard'>My Garden</Link>
+          </li>
+          <li>
+            <Link to='/PlantIndex'>Explore Plants</Link>
+          </li>
+          {/* <li><Link to="/SearchPlants">View All Plants</Link></li>
                 <li><Link to="/UserSearchPlants">Search Plants</Link></li>
                 <li><Link to ="/FlowerSearch">Search By Flower Color</Link></li> */}
-                <Button  color='info' onClick={props.clearToken}>Logout</Button>
-                <NavbarToggler onClick={toggle}/>
-                <Collapse isOpen={isOpen}>
-                </Collapse>
-            </ul>
-        </div>
-        
-        </div>
-     );
-}
- 
+          <Button color='info' onClick={props.clearToken}>
+            Logout
+          </Button>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen}></Collapse>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
 export default Navbar;
