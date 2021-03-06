86c8a3a494abaf130eed9a530a16d6942fc68795
jesnadolski <nadolski.jessica@gmail.com>
2021-03-06 15:00:29 -0500

    deploy heroku

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -1,13 +1,15 @@
-let APIURL = ''
+let APIURL = '';
 
-// console.log(window.location.hostname);
 switch (window.location.hostname) {
-  case 'localhost' || '127.0.0.1':
-    APIURL = 'http://localhost:3001'
-    break
-
-  default:
-    alert('URL Error!')
-    break
+    // this is the local host name of your react app
+    case 'localhost' || '127.0.0.1':
+        // this is the local host name of your API
+        APIURL = 'http://localhost:3001';
+        break;
+    // this is the deployed react application
+    case 'iplants-client.herokuapp.com':
+        // this is the full url of your deployed API
+        APIURL = 'https://iplants-server.herokuapp.com'
 }
-export default APIURL
+
+export default APIURL;
